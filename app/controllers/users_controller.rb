0e8454793ea33b74718be1a238af05c9d5aa75e4
sumi-re01 <owob.viola@gmail.com>
2021-04-03 04:20:47 +0000

    [Add]my_page/ show, edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,2 +1,16 @@
 class Gallery < ApplicationRecord
+
+  belongs_to :user
+  has_many :marks, dependent: :destroy
+
+  validates :title, presence: true
+  validates :text, length: { minimum: 2, maximum: 200 }
+  validates :imge, presence: true
+
+  attachment :image
+
+  def marked_by?
+    marks.where(user_id. user.id).exists?
+  end
+
 end
